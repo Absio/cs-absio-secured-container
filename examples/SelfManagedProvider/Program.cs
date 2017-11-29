dc30edf82c3f55b4d408366350171f8f53bcbfe8
nazar.terletzkiy <nazar.terletzkiy@absio.com>
2017-11-29 20:04:59 +0200

    Created concole application for show how providers work

---
+++ examples/SelfManagedProvider/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Absio.Sdk.Container;
+using Absio.Sdk.Providers;
+
+namespace SelfManagedProvider
+{
+    class Program
+    {
+        internal const string ApiKey = "7b86390a-38af-4ecc-b9b7-2c663ab39bea";
+        internal const string ApplicationName = "AbsioSdk Tests";
+        internal const string ServerUrl = "https://apiint.absio.com";
+        internal static string OfsRoot = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "data";
+
+        static void Main(string[] args)
+        {
+            Work().Wait();
+        }
+
+        private static async Task Work()
+        {
+            //This container is created on the server and in the OFS, but the content is not stored in either location.
+            var selfProvider = new SelfManagedProvider();
+            selfProvider.Initialize(ServerUrl, ApiKey, ApplicationName, OfsRoot);
+
+            await selfProvider.LoginAsync(new Guid("083c92bd-607b-447c-9cd4-6a6294e04250"), "Password#1", "Passphrase#1");
+
+            //Now you, the app developer, decide where to store the data.  Here's an example:
+            var content = "Content";
+            string path = @"d:\temp\MyTest.container";
+            var container = await selfProvider.CreateAsync(Encoding.ASCII.GetBytes(content), "Custom Header");
+
+            using (FileStream fs = File.Open(path, FileMode.OpenOrCreate))
+            {
+                await fs.WriteAsync(container.Bytes(), 0, container.Bytes().Length);
+            }
+
+            //Now to fetch the container back and merge it with your encrypted data:
+            var fileBytes = File.ReadAllBytes(path);
+            var newContainer = await selfProvider.GetAsync(container.Metadata.Id, fileBytes);
+            var content2 = Encoding.ASCII.GetString(newContainer.Content);
+        }
+    }
+}
---
+++ examples/SelfManagedProvider/SelfManagedProvider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Absio.Sdk.Container;
+using Absio.Sdk.Providers;
+
+namespace SelfManagedProvider
+{
+    public class SelfManagedProvider : BaseProvider
+    {
+        public ServerCacheOfsProvider ServerCacheOfsProvider { get; }
+
+        public SelfManagedProvider()
+        {
+            ServerCacheOfsProvider = new ServerCacheOfsProvider();
+        }
+
+        /// <summary>
+        ///     Initialize the Container Provider.
+        /// </summary>
+        public void Initialize(string serverUrl, string apiKey, string applicationName, String ofsRootDirectory = null,
+            bool partitionDataByUser = false)
+        {
+            ServerCacheOfsProvider.Initialize(serverUrl, apiKey, applicationName, ofsRootDirectory,
+                partitionDataByUser);
+        }
+
+        /// <summary>
+        ///     Save the container data so that it is not stored on the server / ofs
+        /// </summary>
+        public async Task<SecuredContainer> CreateAsync(byte[] content, object customHeaderObject = null,
+            List<ContainerAccessLevel> accessLevels = null, string type = null)
+        {
+            var container = ServerCacheOfsProvider.ServerProvider.BuildNewContainer(content, customHeaderObject, accessLevels, type);
+            var securedContainer = await ServerCacheOfsProvider.ContainerEncryptionService.EncryptAsync(container);
+
+            var encryptedData = securedContainer.Bytes();
+            var newContainer = await SecuredContainer.CreateAsync(new byte[0], securedContainer.Metadata);
+
+            await ServerCacheOfsProvider.ServerProvider.SecuredContainerMapper.CreateOrUpdateAsync(newContainer);
+            await ServerCacheOfsProvider.OfsProvider.CreateAsync(newContainer);
+
+            return await SecuredContainer.CreateAsync(encryptedData, newContainer.Metadata);
+        }
+
+        /// <summary>
+        ///     Delete the container keys & access info from the server and ofs.
+        /// </summary>
+        public async Task DeleteAsync(Guid containerId)
+        {
+            await ServerCacheOfsProvider.DeleteUserAsync();
+        }
+
+        /// <summary>
+        ///     Return a container that's as populated as it can be.
+        /// </summary>
+        public async Task<IContainer> GetAsync(Guid containerId, byte[] encryptedData)
+        {
+            var securedContainer = await ServerCacheOfsProvider.ServerProvider.GetSecuredContainerAsync(containerId);
+            var newContainer = await SecuredContainer.CreateAsync(encryptedData, securedContainer.Metadata);
+            return await ServerCacheOfsProvider.ContainerEncryptionService.DecryptAsync(newContainer);
+        }
+
+        public async Task LoginAsync(Guid userId, string password = null, string passphrase = null)
+        {
+            await ServerCacheOfsProvider.LogInAsync(userId, password, passphrase);
+        }
+
+        public void Logout()
+        {
+            ServerCacheOfsProvider.Logout();
+        }
+
+        public async Task RegisterAsync(string password, string passphrase, string reminder = null)
+        {
+            await ServerCacheOfsProvider.RegisterAsync(password, passphrase, reminder);
+        }
+
+    }
+}
